3479119c0b83ecf8538b1df0f908a81e55ae5b9b
haruto-matsushima <matsu_haruto@icloud.com>
2021-08-21 15:54:31 +0900

    initial commit

---
+++ RouterPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RouterPractice
+//
+//  Created by 松島悠人 on 2021/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
