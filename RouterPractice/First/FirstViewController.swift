8fb6ffbe213444e5d0de4ba3aae36d304d45c772
haruto-matsushima <matsu_haruto@icloud.com>
2021-08-22 10:27:12 +0900

    storyboardからの起動を停止

---
+++ RouterPractice/First/FirstViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FirstViewController.swift
+//  RouterPractice
+//
+//  Created by 松島悠人 on 2021/08/22.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
